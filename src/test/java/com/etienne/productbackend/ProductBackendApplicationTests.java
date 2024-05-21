cd00a07f90a0613860787a00130816dc7aa068c7
etienneDev <etienne.baillif97436@gmail.com>
2024-05-21 22:05:31 +0200

    premier commit

---
+++ src/test/java/com/etienne/productbackend/ProductBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.etienne.productbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
